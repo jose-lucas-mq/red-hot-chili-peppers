3fd39453c31dc7930beaa3fc521a0c7f5a4d4eb3
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2020-05-27 12:25:42 -0300

    Novo card da história | tocando música aleatoriamente

--- js/aleatorias.js
+++ js/aleatorias.js
@@ -3,7 +3,19 @@ window.onload = sorteio();
 
 
     function sorteio() {
-       var music = parseInt((Math.random() * 9) + 1);
-       tocando.src = `./../audios/op${music}.mp3`;
-       tocando.volume = 0.01;
+       var music = parseInt((Math.random() * 9));
+       tocando.src = `./audios/op${music}.mp3`;
+
+       var decisao = confirm ("Deseja tocar música?");
+
+
+       if (decisao){
+         tocando.volume = 0.1;
+       }
+       else {
+         tocando.volume = 0;
+         
+       }
+         
+      
     }
